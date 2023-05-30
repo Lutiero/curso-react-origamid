a5ad46f344d5c6be73d94ca0bc1795a558c0064a
lutiero <lutiero@gmail.com>
2023-05-29 21:56:12 -0400

    desafio completo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import Form from "./Form/Form.jsx";
+import Home from "./components/Home.jsx";
+import Product from "./components/Product.jsx";
 
 const App = () => {
+  const {pathname} = window.location;
+  let Component;
+  if (pathname === "/home") {
+    Component = Home;
+  } else {
+    Component = Product;
+  }
+
   return (
     <>
-      <Form />
+      <ul>
+        <li><a href="/home">Home</a></li>
+        <li><a href="/products">Produtos</a></li>
+      </ul>
+      <Component />
     </>
   )
 }
