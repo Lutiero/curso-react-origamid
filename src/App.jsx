f4c7d4076dc3f1c9d7b6cca0eabd97fecb2c80aa
Lutiero <lutiero@gmail.com>
2023-05-30 13:52:08 -0400

    exercicio com fetch e useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,32 @@
-import Home from "./components/Home.jsx";
-import Product from "./components/Product.jsx";
+import React from "react";
+import Produto from "./components/Produto.jsx";
+
 
 const App = () => {
-  const {pathname} = window.location;
-  let Component;
-  if (pathname === "/home") {
-    Component = Home;
-  } else {
-    Component = Product;
-  }
+    const [dados, setDados] = React.useState(null);
+    const [loading, setLoading] = React.useState(null);
 
-  return (
-    <>
-      <ul>
-        <li><a href="/home">Home</a></li>
-        <li><a href="/products">Produtos</a></li>
-      </ul>
-      <Component />
-    </>
-  )
+    function handleClick(event) {
+        let url = `https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`;
+        setLoading(true);
+        fetch(url)
+            .then((response) => response.json())
+            .then((json) => {
+                setDados(json);
+                setLoading(false);
+            });
+
+
+    }
+
+
+    return (<>
+            <button style={{margin: "0.5rem"}} onClick={handleClick}>notebook</button>
+            <button style={{margin: "0.5rem"}} onClick={handleClick}>smartphone</button>
+            <button style={{margin: "0.5rem"}} onClick={handleClick}>tablet</button>
+            {loading && <p>Carregando...</p>}
+            {!loading && dados && <Produto dados={dados}/>}
+        </>)
 }
 
 export default App;
