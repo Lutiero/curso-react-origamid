f4f1c4d4c2421e8cfeb2aa089c47ab5894626711
Lutiero <lutiero@gmail.com>
2023-05-31 16:56:15 -0400

    envio de dados para api, criando um usuario

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,104 @@
-import React from "react";
-import useLocalStorage from "./Hooks/useLocalStorage.jsx";
-import useFetch from "./Hooks/useFetch.jsx";
+import React, {useState} from "react";
 
+const formFields = [
+    {
+        id: 'nome',
+        label: 'Nome',
+        type: 'text'
+    },
+    {
+        id: 'email',
+        label: 'Email',
+        type: 'email'
+    },
+    {
+        id: 'senha',
+        label: 'Senha',
+        type: 'password'
+    },
+    {
+        id: 'cep',
+        label: 'CEP',
+        type: 'text'
+    },
+    {
+        id: 'rua',
+        label: 'Rua',
+        type: 'text'
+    },
+    {
+        id: 'numero',
+        label: 'Número',
+        type: 'text'
+    },
+    {
+        id: 'bairro',
+        label: 'Bairro',
+        type: 'text'
+    }, {
+        id: 'cidade',
+        label: 'Cidade',
+        type: 'text'
+    }, {
+        id: 'estado',
+        label: 'Estado',
+        type: 'text'
+    },
+]
 
 const App = () => {
-    const [produto, setProduto] = useLocalStorage('produto', '');
-    const {request, data, loading, error} = useFetch();
+    const [form, setForm] = React.useState({
+        nome: '',
+        email: '',
+        senha: '',
+        cep: '',
+        rua: '',
+        numero: '',
+        bairro: '',
+        cidade: '',
+        estado: '',
+    });
+    const [resposta, setResposta] = useState(null);
 
-    React.useEffect(() => {
-        async function fetchData() {
-            const {response, json} = await request('https://ranekapi.origamid.dev/json/api/produto/')
-            console.log(response)
-            console.log(json)
-        }
-        fetchData();
-    }, [request]);
 
-    function handleClick({target}) {
-        setProduto(target.innerText);
+    function handleSubmit(event) {
+        event.preventDefault();
+        fetch('https://ranekapi.origamid.dev/json/api/usuario', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(form),
+        }).then((response) => {
+            setResposta(response);
+        })
     }
 
-    if (loading) return <p>CARREGANDO...</p>;
-    if (error) return <h1>ERROR</h1>;
-    if (data)
-        return (<div>
-            <button onClick={handleClick}>notebook</button>
-            <button onClick={handleClick}>smartphone</button>
-            <hr/>
-            {data.map((produto) => (
-                <li key={produto.id}>{produto.nome}</li>
+    function handleChange({target}) {
+        const {id, value} = target;
+        setForm({...form, [id]: value});
+    }
+
+
+    return (<div>
+        <form onSubmit={handleSubmit}>
+            {formFields.map(({id, label, type}) => (
+                <div>
+                    <label htmlFor={id}>{label}</label>
+                    <input
+                        id={id}
+                        type={type}
+                        name={id}
+                        value={form[id]}
+                        onChange={handleChange}
+                    />
+                </div>
             ))}
-        </div>)
-    else return null;
+            <button>Enviar</button>
+        </form>
+        {resposta && 'Usuario criado'}
+    </div>)
+
 }
 
 export default App;
